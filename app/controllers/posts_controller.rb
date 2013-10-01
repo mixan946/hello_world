0bd283aca8ea63e0b92632fb4ebea82074f839d1
Mikhail <mixan946@gmail.com>
2013-10-01 11:23:44 +0400

    CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,40 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
   end
 
-  def create
-    render text: params[:post].inspect
+  def edit
+    @post = Post.find(params[:id])
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to post_path(@post.id)
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params[:post].permit(:title, :text)
+  end
+
 end
